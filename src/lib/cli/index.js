13a6f16c5ef16ce790adaa0f329d71a7420ffd60
mtti <mtti@users.noreply.github.com>
2018-02-23 20:31:44 +0200

    Remove base project and init command

--- src/lib/cli/index.js
+++ src/lib/cli/index.js
@@ -1,12 +1,10 @@
 const generate = require('./generate');
-const init = require('./init');
 const serve = require('./serve');
 const createPost = require('./create-post');
 
 module.exports = {
   commands: {
     generate,
-    init,
     serve,
     'create-post': createPost,
   },
