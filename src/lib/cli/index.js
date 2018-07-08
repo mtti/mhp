978fdf53141d227db1ff4a26d3507904e2d3ffa5
mtti <mtti@users.noreply.github.com>
2018-07-08 16:35:14 +0300

    Add command for generating random posts for development

--- src/lib/cli/index.js
+++ src/lib/cli/index.js
@@ -1,11 +1,13 @@
 const serve = require('./serve');
 const createPost = require('./create-post');
 const generateRoutes = require('./generate-routes');
+const generatePosts = require('./generate-posts');
 
 module.exports = {
   commands: {
     serve,
     'create-post': createPost,
     'generate-routes': generateRoutes,
+    'generate-posts': generatePosts,
   },
 };
