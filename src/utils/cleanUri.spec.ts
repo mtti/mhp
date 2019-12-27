087751d90b5208462ee88e4c23255b366289a7f4
mtti <mtti@users.noreply.github.com>
2019-12-27 16:48:32 +0200

    Add more cleanUri tests

--- src/utils/cleanUri.spec.ts
+++ src/utils/cleanUri.spec.ts
@@ -5,4 +5,19 @@ describe(cleanUri.name, () => {
     const result = cleanUri([]);
     expect(result.length).toBe(0);
   });
+
+  it('removes extension from regular paths', () => {
+    const result = cleanUri(['foo', 'bar', 'baz.html']);
+    expect(result).toEqual(['foo', 'bar', 'baz']);
+  });
+
+  it('removes index.html entirely', () => {
+    const result = cleanUri(['foo', 'bar', 'index.html']);
+    expect(result).toEqual(['foo', 'bar']);
+  });
+
+  it('does not change non-html extension', () => {
+    const result = cleanUri(['foo', 'bar', 'baz.txt']);
+    expect(result).toEqual(['foo', 'bar', 'baz.txt']);
+  });
 });
