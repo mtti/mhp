bfee851b71bd7dcd73ec7e5a0e26759899edd78a
mtti <mtti@users.noreply.github.com>
2020-12-12 19:51:33 +0200

    Fix index middleware types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './build';
 export * from './loadPosts';
 export * from './middleware';
+export * from './Post';
 export * from './preprocessors';
 export * from './sort';
 export * from './types';
